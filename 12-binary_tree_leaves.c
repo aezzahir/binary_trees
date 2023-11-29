b280e43f2ae4a6a1215ab439def285994cbcf87b
oussamaNAJIH <oussamanajih6@gmail.com>
2023-11-29 14:26:53 +0100

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,7 +10,8 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0; 
+	size_t count = 0;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL || tree->right == NULL)
