3d4b48f5c6e5690a2b503b7ee11f01f6c5027efc
oussamaNAJIH <oussamanajih6@gmail.com>
2023-11-29 19:22:21 +0100

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,13 +10,16 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-size_t count = 0;
-if (tree == NULL)
-    return (0);
-if (tree->left != NULL || tree->right != NULL)
-{
-    count++;
-    return (count);
-}
-return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		count++;
+		return (count);
+	}
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+	return (count);
 }
